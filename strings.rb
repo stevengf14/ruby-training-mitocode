370f1f8f6b0b1703d4e0709e154a69cbd50b7566
Steven <stevengf14@gmail.com>
2023-01-31 18:10:53 -0500

    strings

---
+++ strings.rb
@@ -0,0 +1,10 @@
+puts "Ruby basic course"
+puts "this is a " + "mitocode course"
+
+# Iteration
+puts "Ruby basic course iteration\n" * 4
+
+# With numbers
+puts 12 + 12
+puts "12" + "12"
+puts "12 + 12"
