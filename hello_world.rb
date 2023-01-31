aec95de1acf10001cabd935f3f4445f31957bc00
Steven <stevengf14@gmail.com>
2023-01-31 18:04:36 -0500

    different files

--- hello_world.rb
+++ hello_world.rb
@@ -1,21 +1,2 @@
 puts "Hello World!"
 # print "Hello World!"
-
-
-# Numbers
-
-# Addition / Substraction
-puts 9 + 2
-puts 9.0 + 2.0
-puts 9 - 2
-
-# Division
-puts 9 / 2
-puts 9.0 / 2 
-
-# Multiplication
-puts 9 * 3
-puts 9.0 * 3
-
-# Power
-puts 9**2
---
+++ numbers.rb
@@ -0,0 +1,17 @@
+# Numbers
+
+# Addition / Substraction
+puts 9 + 2
+puts 9.0 + 2.0
+puts 9 - 2
+
+# Division
+puts 9 / 2
+puts 9.0 / 2 
+
+# Multiplication
+puts 9 * 3
+puts 9.0 * 3
+
+# Power
+puts 9**2
