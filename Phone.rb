7d7c1ef7eff5c4653b14fd76b0fc041765294321
Steven <stevengf14@gmail.com>
2023-02-15 22:28:42 -0500

    accesories

--- Phone.rb
+++ Phone.rb
@@ -9,6 +9,25 @@ class Phone
         puts @model + " - " + @color + " is calling..."
     end
 
+    # getters and setters
+    def model
+        return @model
+    end
+
+    def color
+        return @color
+    end
+
+    def model=(model)
+        @model = model
+    end
+
+    def color=(color)
+        @color = color
+    end
+
+    # attr_accessor :model, :color
+
 end
 
 phone1 = Phone.new("Xiaomi redmi 12 pro", "Black")
@@ -16,3 +35,8 @@ phone1.call()
 
 phone2 = Phone.new("Iphone 12 mini", "White")
 phone2.call()
+
+phone3 = Phone.new("Motorola", "Black")
+phone3.model = "Nokia 1100"
+phone3.color = "White"
+phone3.call()
